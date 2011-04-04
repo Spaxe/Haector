0a91d2113a8900ffbb19791f38091a86ba1f3f8a
Xavier Ho <contact@xavierho.com>
2011-04-05 03:31:12 +1000

    Working on operator overloading in Geo

--- Geo.hs
+++ Geo.hs
@@ -22,3 +22,33 @@ data Vec3 a = Vec3 a a a
 
 instance (Num a) => Num (Vec3 a) where
   (Vec3 i j k) + (Vec3 u v w) = Vec3 (i+u) (j+v) (k+w)
+  _ * _ = error "(*) is undefined for Vec3"
+  
+  
+-----------------------------------------------------------
+-- Unit Testing
+-----------------------------------------------------------
+same input expected | input == expected = (0, "Passed")
+                    | otherwise         = (1, "Failed: " ++ show input ++ " == " ++ show expected)
+    
+diff input expected | input /= expected = (0, "Passed")
+                    | otherwise         = (1, "Failed: " ++ show input ++ " /= " ++ show expected)
+  
+test = do
+  -- Insert unit test here
+  let trials = same (x+y) (y+x)
+             : diff (x+y) (x+y)
+             : diff (3*3) (x+y)
+             : []
+
+  --------------------------
+  let failed = sum $ map fst trials
+  let msgs = ["  " ++ msg | (flag, msg) <- trials, flag /= 0]
+  let passed = length trials - failed
+  putStrLn "Geo.hs:"
+  putStr $ unlines msgs
+  putStrLn ("     " ++ show passed ++ " passed, " ++ show failed ++ " failed.")
+    where -- Insert variables below
+      x = Vec3 1 0 0
+      y = Vec3 0 1 0
+      z = Vec3 0 0 1
