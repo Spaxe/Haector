2b913b4964395441779814aafce1adf1e4d5956e
Xavier Ho <contact@xavierho.com>
2011-04-05 00:57:44 +1000

    Added Shape2D.hs

---
+++ Shape2D.hs
@@ -0,0 +1,13 @@
+{- |
+Module      :  $Header$
+Description :  Type definitions for 2D shapes.
+Copyright   :  (C) 2011 by Xavier Ho
+License     :  MIT License
+
+Maintainer  :  contact@xavierho.com
+Stability   :  experimental
+Portability :  portable
+
+Definitions for simple 2D shapes such as rectangles and lines.
+-}
+
